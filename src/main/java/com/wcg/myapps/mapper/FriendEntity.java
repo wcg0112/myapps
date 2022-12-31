bab284d7f93df6d60bfc0898a290fe41a0852bdd
wcg <wcg0112@gmail.com>
2022-12-31 17:23:36 +0900

    new

---
+++ src/main/java/com/wcg/myapps/mapper/FriendEntity.java
@@ -0,0 +1,11 @@
+package com.wcg.myapps.mapper;
+
+import lombok.Data;
+
+@Data
+public class FriendEntity {
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+}
